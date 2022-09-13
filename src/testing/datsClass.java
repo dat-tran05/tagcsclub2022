ddab9c80582516fd07f94e962f4b7ce16214b465
chicken-04 <datctran2005@gmail.com>
2022-09-12 21:13:34 -0500

    first commit

---
+++ src/testing/datsClass.java
@@ -0,0 +1,12 @@
+package testing;
+
+import java.util.Iterator;
+import java.util.*;
+public class datsClass {
+	public static void main(String[] args) {
+		System.out.println('d');
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+	}
+}
