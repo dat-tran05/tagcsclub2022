7e3ac0cb752d14711011ba2528cc6be2ec054d3a
dat <99101704+dat-tran05@users.noreply.github.com>
2022-11-04 16:08:10 -0500

    sdf

--- src/testing/datsClass.java
+++ src/testing/datsClass.java
@@ -1,12 +1,13 @@
 package testing;
 
-import java.util.Iterator;
 import java.util.*;
+
 public class datsClass {
 	public static void main(String[] args) {
+		Stack<String> stack = new Stack<>();
 		System.out.println('d');
 		for (int i = 0; i < args.length; i++) {
-			
+			stack.add("'s");
 		}
 	}
 }
