2622f04639c76bb97fa48bbf1299f2448259b0b0
dat <99101704+dat-tran05@users.noreply.github.com>
2023-01-25 16:13:18 -0600

    january24

--- src/testing/datsClass.java
+++ src/testing/datsClass.java
@@ -1,12 +1,50 @@
 package testing;
 
 import java.util.Iterator;
+import java.util.function.Consumer;
+import java.util.function.*;
 import java.util.*;
 public class datsClass {
 	public static void main(String[] args) {
-		System.out.println('d');
-		for (int i = 0; i < args.length; i++) {
-			
+//		List<Apple> list = new ArrayList<>();
+//		for(int i =0 ; i < 10; i++) {
+//			list.add(new Apple());
+//		}
+		long longer = (System.currentTimeMillis());
+		var a1 = new A();
+		var b = "sdf";
+		var  c1 = 1;
+		var a23234 = new int[]{1,2,3,4};
+		System.out.println(Arrays.toString(a23234));
+		var sixty = new Stack<String>();
+		sixty.push("dfs");
+		System.out.println(sixty);
+		sixty = new Stack<String>();
+//		Apple a = list.get(0);
+//		System.out.println(a.total);
+		Consumer<String> c = (n) -> System.out.println(n);
+//		BiConsumer<String,String> bc = (d,b) -> System.out.println(d+"\n" + b);
+		Predicate<Integer> p = (s) -> {
+			 return s ==1 ;
+			 
+		};
+		List<String> list2 = Arrays.asList("sdf", "sdfs","sdfs");
+		list2.forEach(System.out::println);
+		c.accept("sdf");
+		
+		for(;c1 < 1000;) {
+			System.out.println(c1++);
 		}
+		System.out.println(longer);
+		System.out.println((System.currentTimeMillis()-longer)/1000.0);
+
+//		bc.accept("apple", "banana");
+//		System.out.println(\);
+	}
+}
+class A{
+	public static int total;	
+	public A() {
+		total++;
 	}
 }
--- src/testing/food.java
+++ src/testing/food.java
@@ -1,41 +1,135 @@
-package testing;
-
 import java.util.*;
 import java.io.*;
-import java. math.*;
+import java.math.*;
 
-public class food {
-	public static void main(String[] datt) throws Throwable{
-		Scanner sc = new Scanner(new File("src/testing/test2.dat"));
-		List<Book> list = new ArrayList<>();
-		while(sc.hasNextLine())
-			list.add(new Book(sc.nextLine()));
-//		Collections.sort(list);
-		//sorts the same as the previous example
-		list.sort(Comparator.comparing(Book::getAuthor).thenComparing(Comparator.comparing(Book::getGenre)));
-		
-		list.forEach(System.out::println);
+public class food extends PrintWriter {
+	food() {
+		super(System.out);
 	}
-}
-class Book
-{
-	public String author, name, genre;
-	public Book(String line)
-	{
-		String[] arr = line.split("\\*");
-		name = arr[0];
-		author = arr[1];
-		genre = arr[2];
+
+	FastScanner sc = new FastScanner();
+
+	public static void main(String[] nutz) throws IOException {
+		food o = new food();
+		o.main();
+		o.flush()
 	}
-	public String toString()
-	{
-		return name+ "*" + author + "*" + genre;
+
+	void main() {
+		for (int t = sc.nextInt(); t-- > 0;) {
+			println("sdf");
+			printf("%f",20);
+		}
 	}
-	public String getAuthor() {
-		return author;
-	}
-	public String getGenre()
-	{
-		return genre;
+
+	static class FastScanner extends PrintWriter {
+		private InputStream stream;
+		private byte[] buf = new byte[1 << 16];
+		private int curChar, numChars;
+
+		// standard input
+		public FastScanner() {
+			this(System.in, System.out);
+		}
+
+		public FastScanner(InputStream i, OutputStream o) {
+			super(o);
+			stream = i;
+		}
+
+		// file input
+		public FastScanner(String i, String o) throws IOException {
+			super(new FileWriter(o));
+			stream = new FileInputStream(i);
+		}
+
+		// throws InputMismatchException() if previously detected end of file
+		private int nextByte() {
+			if (numChars == -1)
+				throw new InputMismatchException();
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars == -1)
+					return -1; // end of file
+			}
+			return buf[curChar++];
+		}
+
+		// to read in entire lines, replace c <= ' '
+		// with a function that checks whether c is a line break
+		public String next() {
+			int c;
+			do {
+				c = nextByte();
+			} while (c <= ' ');
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = nextByte();
+			} while (c > ' ');
+			return res.toString();
+		}
+
+		public String nextLine() {
+			int c;
+			do {
+				c = nextByte();
+			} while (c < '\n');
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = nextByte();
+			} while (c > '\n');
+			return res.toString();
+		}
+
+		public int nextInt() {
+			int c;
+			do {
+				c = nextByte();
+			} while (c <= ' ');
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = nextByte();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res = 10 * res + c - '0';
+				c = nextByte();
+			} while (c > ' ');
+			return res * sgn;
+		}
+
+		public long nextLong() {
+			int c;
+			do {
+				c = nextByte();
+			} while (c <= ' ');
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = nextByte();
+			}
+			long res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res = 10 * res + c - '0';
+				c = nextByte();
+			} while (c > ' ');
+			return res * sgn;
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
 	}
 }
